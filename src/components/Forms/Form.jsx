4c186ace0e08669fc42998f49cd481c18f8a4684
Cibele Martins <93346472+CibeleMartins@users.noreply.github.com>
2022-05-23 15:06:36 -0300

    :package: feat: 'fix:

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,54 +1,77 @@
 import React from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
 import styles from './Form.module.css';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 
 export default function Form({btnText}) {
 
   let navigate = useNavigate()
 
-  const {register, handleSubmit, formState: { errors }} = useForm()
+  const [nome, setNome] = useState('')
+  
+  const [lt, setLt] = useState('')
+  
+  const [lg, setLg] = useState('')
 
-  const addPost = data => axios.post("https://localityapi.herokuapp.com/localitys", data)
-  .then(() => {
-      console.log("Deu tudo certo")
-      navigate("/allMap")
-  })
-  .catch((error) => {
-      console.log(error)
-  })
+
+  const addPost = (event) => {
+
+    event.preventDefault()
+    
+    axios.post("https://localityapi.herokuapp.com/localitys", 
+  {
+    nome: nome,
+    latitude: lt,
+    longitude: lg
+  }).then((data) => {
+    console.log("Deu tudo certo")
+    console.log(data)
+    navigate("/allMap")
+})
+.catch((error) => {
+    console.log(error)
+})
+
+}
+  
 
 return(
 
         <div className={styles.div}>
 
-          <form className={styles.form} onSubmit={handleSubmit(addPost)}>
+          <form className={styles.form} onSubmit={addPost}>
 
             <Input 
             className={styles.input}
             type="text" 
             placeholder="Localidade" 
-            name='nome' 
-            {...register('nome')}/>
-            <p className={styles.error_message}>{errors.content?.message}</p>
+            name='nome'
+            value={nome} 
+            onChange={(event)=> setNome(event.target.value)}
+           />
+           
           
             <Input 
             type="text" 
             name='latitude' 
-            placeholder="Latitude" 
-            {...register('latitude')}/>
-            <p className={styles.error_message}>{errors.content?.message}</p>
+            placeholder="Latitude"
+            value={lt} 
+            onChange={(event)=> setLt(event.target.value)}
+            />
+            
 
             <Input 
             type="text" 
             name="longitude" 
             placeholder="Longitude" 
-            {...register('longitude')}/>
-            <p className={styles.error_message}>{errors.content?.message}</p>
+            value={lg}
+            onChange={(event)=> setLg(event.target.value)}
+            />
+          
           
             <Button type="submit" text='Cadastrar'
             onClick={()=> console.log('clicado')}/>
